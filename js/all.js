b6557124cda0768662d4552ef3f3b08647f86ea8
rock880803 <rock880803@gmail.com>
2022-05-18 21:31:43 +0800

    add: media

---
+++ js/all.js
@@ -0,0 +1,12 @@
+const $btn1 = $('.nav-btn').eq(0);
+const $btn2 = $('.nav-btn').eq(1);
+const $btn3 = $('.nav-btn').eq(2);
+const $btn4 = $('.nav-btn').eq(3);
+const $btn5 = $('.nav-btn').eq(4);
+const $btn6 = $('.nav-btn').eq(5);
+
+
+$btn1.click(()=>{
+console.log(0);
+
+});

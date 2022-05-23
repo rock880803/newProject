78005dcf2fb71ded6906984da8209ff87e452d50
rock880803 <rock880803@gmail.com>
2022-05-23 21:37:22 +0800

    add: RWD

--- js/all.js
+++ js/all.js
@@ -1,12 +1,22 @@
+
+const isMobile = $(window).width() <= 480;
 const $btn1 = $('.nav-btn').eq(0);
 const $btn2 = $('.nav-btn').eq(1);
 const $btn3 = $('.nav-btn').eq(2);
 const $btn4 = $('.nav-btn').eq(3);
 const $btn5 = $('.nav-btn').eq(4);
 const $btn6 = $('.nav-btn').eq(5);
+// console.log(isMobile);
 
+$btn1.addClass('js-nav-btn');
 
-$btn1.click(()=>{
-console.log(0);
-
+$('.nav-btn').click(function(){
+    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
 });
+
+if(isMobile){
+    $('#SlideLs .slide-item').scroll(function(){
+        console.log($(this).scrollTop());
+        $(this).scrollTop() == 0 ? $('#Nav').removeClass('js-nav') :  $('#Nav').addClass('js-nav') 
+    });
+}
